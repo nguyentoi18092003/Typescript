d98b41fea201353a90cafd62df106d4e2a866dc9
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-10-26 05:21:01 +0700

    Set up: watch thay doi tu dong +test thu

---
+++ 01_Introduce.ts
@@ -0,0 +1,27 @@
+let addres: string ='Ho Chi Minh';
+console.log(showAddress(addres));
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+console.log("6");
+console.log("7");
+console.log("8");
+console.log("9");
+console.log("10");
+console.log("11");
+console.log("12");
+console.log("14");
+console.log("15");
+console.log("17");
+
+
+
+
+
+
+function showAddress(address: string){
+    return address;
+}
+ 
---
+++ out-tsc/e2e/01_Introduce.js
@@ -0,0 +1,21 @@
+var addres = 'Ho Chi Minh';
+console.log(showAddress(addres));
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+console.log("6");
+console.log("7");
+console.log("8");
+console.log("9");
+console.log("10");
+console.log("11");
+console.log("12");
+console.log("14");
+console.log("15");
+console.log("17");
+function showAddress(address) {
+    return address;
+}
+//# sourceMappingURL=01_Introduce.js.map
