20c2453a92e686bff314fb9c7bc0e9ad355aaadf
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-10-26 19:59:16 +0700

    String va cac phuong thuc cua String giong java 99%

---
+++ 09_String.ts
@@ -0,0 +1,35 @@
+import { connected } from "process";
+
+let cityName: string='Ho Chi Minh';
+console.log(cityName);
+
+let stateName: string ="Thu duc";
+console.log(stateName);
+
+//Before ES6
+console.log(cityName +"-"+stateName);
+
+//After ES6
+console.log(`${cityName}-${stateName}`);
+
+//senkey to textbox
+let debugValue ="Debugger"+
+"\n listening"+
+"\n on";
+console.log(debugValue);
+
+//Method
+//charAt()
+//Concat()
+//indexOf()
+//replace()
+//split()
+//toUpperCase()
+//toLowerCase()
+//includes()
+//endsWith()
+//startsWith()
+//substr()
+//substring()
+//trim(),trimLeft(), trimRight()
+
