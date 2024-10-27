330f7ab44102ed87d8007c2a0246cf72c6e7b51a
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-10-27 20:24:42 +0700

    Let var const

---
+++ 02_Scope.ts
@@ -0,0 +1,17 @@
+//Global scope: Dung duoc cho toan chuong trinh
+
+//Local scope
+
+//Block scope(if/ for/switch)
+
+// hosting dùng trước, khai báo sau
+// Neu define mot bien let trong ham hoac bllock thi ra ngoai se khong dung dc, con var thi van dung duoc
+// var con cho phep define ten bien giong nhau
+// Hay dung let vi var qua rong mk khong controll duoc
+
+//Neu khai bao 2 bien cung ten voi tu khoa var- mot bien khai bao trong mot khoi cha- mot bien trong khoi con, chuowng trinh se coi nhu la 1 bien(vi sung 1 vi tri tren bo nho), va chang han no duoc console.log() ra thi no se lay cai duoc khai bao sau
+
+
+//CONST
+//LET: Pham vi khoi
+//VAR: co pham vi trong function,han che dung var
